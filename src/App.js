3855e38e2a7c5d57134fd95857c56f40916f7d0a
Bobby W Bryant <sirbob1999@gmail.com>
2020-04-01 13:42:02 -0400

    changed out App.js and added UserCard and UserCardList

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import UserCardList from './components/UserCardList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    textInput: "",
+    users: []
+  };
+
+  handleChange = event => {
+    this.setState({
+      textInput: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const userName = this.state.textInput;
+    fetch(`https://api.github.com/users/${userName}`)
+    .then(res => res.json())
+    .then((result) => {
+        this.setState({
+            textInput: "",
+            users: [...this.state.users,result]
+        });
+    });
+  };
+
+  render() {
+    const {users} = this.state;
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Enter user Name:
+            <input
+              type="text"
+              value={this.state.textInput}
+              placeholder="Text Input"
+              onChange={this.handleChange}
+            />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+        <UserCardList 
+          users={users}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
